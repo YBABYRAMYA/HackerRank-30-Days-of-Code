b71685e19ff6923b6f658e3a370243a2b8ab2106
YBABYRAMYA <142166288+YBABYRAMYA@users.noreply.github.com>
2024-04-26 22:52:08 +0530

    Create Day 1: Data Types.py
    Python program for Day 1: Data Types

---
+++ Day 1: Data Types.py
@@ -0,0 +1,10 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+ii=int(input())
+dd=float(input())
+ss=input()
+print(i + ii)
+print(d + dd)
+print(s + ss)
