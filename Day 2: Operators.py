921d5356a3060c4d90a8ad0509189f2d651d578e
YBABYRAMYA <142166288+YBABYRAMYA@users.noreply.github.com>
2024-04-26 22:54:03 +0530

    Create Day 2: Operators.py
    Python program for Day 2: Operators

---
+++ Day 2: Operators.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'solve' function below.
+#
+# The function accepts following parameters:
+#  1. DOUBLE meal_cost
+#  2. INTEGER tip_percent
+#  3. INTEGER tax_percent
+#
+
+def solve(meal_cost, tip_percent, tax_percent):
+    tip_percent=meal_cost/100*tip_percent
+    tax_percent=tax_percent/100*meal_cost
+    total_cost=meal_cost+tip_percent+tax_percent
+    print(f"%0.0f"%total_cost)
+
+if __name__ == '__main__':
+    meal_cost = float(input().strip())
+
+    tip_percent = int(input().strip())
+
+    tax_percent = int(input().strip())
+
+    solve(meal_cost, tip_percent, tax_percent)
